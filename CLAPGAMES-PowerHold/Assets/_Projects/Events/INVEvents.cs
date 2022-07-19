cd23389a129ff35d498fd7d3f335d4d9d8471286
Ozgur <aymelekozgurr53@gmail.com>
2022-07-19 09:07:29 +0300

    inv

---
+++ CLAPGAMES-PowerHold/Assets/_Projects/Events/INVEvents.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CG_PowerHold
+{
+    public class INVEvents : MonoBehaviour
+    {
+        
+    }
+}
+
