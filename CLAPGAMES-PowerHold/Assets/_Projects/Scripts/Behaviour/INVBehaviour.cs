1ef57abc3e6be4f092b560e7d65531207bd4ae5e
Ozgur <aymelekozgurr53@gmail.com>
2022-07-19 10:30:35 +0300

    inv

--- CLAPGAMES-PowerHold/Assets/_Projects/Scripts/Behaviour/INVBehaviour.cs
+++ CLAPGAMES-PowerHold/Assets/_Projects/Scripts/Behaviour/INVBehaviour.cs
@@ -12,11 +12,14 @@ public class INVBehaviour : MonoBehaviour
     public float sensitivity;
     [SerializeField] private float boundX;
     [SerializeField] private bool isPressing = false;
+    private float _screenWidthMultiplier;
 
     private void Awake()
     {
         GetComponents();
         SubscribeEvents();
+
+        _screenWidthMultiplier = 1.0f / Screen.width;
     }
 
     private void OnDestroy()
@@ -26,9 +29,58 @@ public class INVBehaviour : MonoBehaviour
 
     private void SubscribeEvents()
     {
+        INVEvents.OnStart += OnStart;
+        INVEvents.OnInteractWithEnemy += OnInteractWithEnemy;
+        INVEvents.OnJump += OnJump;
+        INVEvents.OnHold += OnHold;
+        INVEvents.OnRelease += OnRelease;
+
+        INVInputs.PointerMoved += OnPointerMoved;
+        INVInputs.PointerPressed += OnPointerPressed;
+        INVInputs.PointerRemoved += OnPointerRemoved;
+    }
+
+    private void OnPointerRemoved(Vector3 obj)
+    {
         
     }
-    
+
+    private void OnPointerPressed(Vector3 obj)
+    {
+        
+    }
+
+    private void OnPointerMoved(Vector3 mouseMovementDirection)
+    {
+        BasicMoveX(mouseMovementDirection);
+    }
+
+    private void OnRelease()
+    {
+        
+    }
+
+    private void OnHold()
+    {
+        
+    }
+
+    private void OnJump()
+    {
+        
+    }
+
+    //TODO: karakterimizin leveli yükselecek ve düşmanlar ölecek.
+    private void OnInteractWithEnemy()
+    {
+        
+    }
+
+    private void OnStart()
+    {
+        INVEvents.OnUpdate += MoveForward;
+    }
+
     private void UnsubscribeEvents()
     {
         
@@ -44,6 +96,22 @@ public class INVBehaviour : MonoBehaviour
     private void MoveForward()
     {
         transform.position += Vector3.forward * Time.deltaTime * playerSpeed;
+    }
+
+    private void BasicMoveX(Vector3 mouseMovementDirection)
+    {
+        Vector3 mouseToWorldDirection = new Vector3(mouseMovementDirection.x * _screenWidthMultiplier, 0f, 0f);
+        Vector3 addVector = mouseToWorldDirection * Time.deltaTime * sensitivity;
+
+        transform.position += addVector;
+
+        Transform thisTransform = transform;
+
+        if (thisTransform.position.x < -boundX)
+            thisTransform.position = new Vector3(-boundX, thisTransform.position.y, thisTransform.position.z);
+        
+        if (thisTransform.position.x > boundX)
+            thisTransform.position = new Vector3(boundX, thisTransform.position.y, thisTransform.position.z);
     }
 
     #endregion
---
+++ CLAPGAMES-PowerHold/Assets/_Projects/Scripts/Inputs/INVInputs.cs
@@ -0,0 +1,46 @@
+using System;
+using UnityEngine;
+
+public class INVInputs : MonoBehaviour
+{
+    public static event Action<Vector3> PointerPressed;
+    public static event Action<Vector3> PointerMoved;
+    public static event Action<Vector3> PointerRemoved;
+
+    [Header("Settings")] private Vector3 lastMousePosition;
+
+    private void Start()
+    {
+        INVEvents.OnStart += OnStart;
+    }
+
+    private void OnStart()
+    {
+        INVEvents.OnUpdate += OnUpdate;
+    }
+
+    private void OnUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastMousePosition = Input.mousePosition;
+            PointerPressed?.Invoke(lastMousePosition);
+        }
+        
+        if (Input.GetMouseButton(0))
+        {
+            var currentMousePosition = Input.mousePosition;
+
+            if (lastMousePosition != currentMousePosition)
+            {
+                PointerMoved?.Invoke(currentMousePosition - lastMousePosition);
+                lastMousePosition = currentMousePosition;
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            PointerRemoved?.Invoke(Input.mousePosition);
+        }
+    }
+}
