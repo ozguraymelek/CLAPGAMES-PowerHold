d5ecd87f67907da1e6cfe26a727a5c9d63e24852
Ozgur <aymelekozgurr53@gmail.com>
2022-07-19 09:52:53 +0300

    inv

--- CLAPGAMES-PowerHold/Assets/_Projects/Events/INVEvents.cs
+++ CLAPGAMES-PowerHold/Assets/_Projects/Events/INVEvents.cs
@@ -1,12 +1,57 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-namespace CG_PowerHold
+public class INVEvents : MonoBehaviour
 {
-    public class INVEvents : MonoBehaviour
+    /// <summary>
+    ///     OnSuccess --> Arenalar 5 bölümden oluşuyor. Arenanın son bölümündeki boss'un levelinden yüksekse yeni arenaya geçilir.
+    ///     OnFail --> Engellere çarparsa bölüm yeniden başlar.
+    ///     OnHold --> Karakterin saldırı mekaniği ve animasyonu
+    ///     OnRelease --> Normal koşu
+    ///     OnJump --> Sıradaki platforma zıplamak.
+    ///     OnInteractWithEnemy --> Karakterin leveli yükselecek ve düşmanlar ölecek.
+    /// </summary>
+    public static event Action OnStart,
+        OnUpdate,
+        OnFixedUpdate,
+        OnSuccess,
+        OnFail,
+        OnHold,
+        OnRelease,
+        OnJump,
+        OnInteractWithEnemy;
+
+    private void Awake()
     {
-        
+        Reset();
+    }
+
+    private void Reset()
+    {
+        OnStart = null;
+        OnUpdate = null;
+        OnFixedUpdate = null;
+        OnSuccess = null;
+        OnFail = null;
+        OnHold = null;
+        OnRelease = null;
+        OnJump = null;
+        OnInteractWithEnemy = null;
+    }
+
+    public void OnStartButtonClicked()
+    {
+        OnStart?.Invoke();
+    }
+
+    private void Update()
+    {
+        OnUpdate?.Invoke();
+    }
+
+    private void FixedUpdate()
+    {
+        OnFixedUpdate?.Invoke();
     }
 }
 
