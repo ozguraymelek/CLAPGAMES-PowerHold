4bb94a26c2416b1e111c0a450b9f9fcfc97d511e
Ozgur <aymelekozgurr53@gmail.com>
2022-07-22 15:59:52 +0300

    inv

--- CLAPGAMES-PowerHold/Assets/000/ExampleHead.cs
+++ CLAPGAMES-PowerHold/Assets/000/ExampleHead.cs
@@ -10,8 +10,29 @@ public class ExampleHead : Singleton<ExampleHead>
 
     public void DismemberAllRig()
     {
+        #region Lower Body
+        
         _dismemberment.Dismember("R_Thigh");
         _dismemberment.Dismember("R_Calf");
+        
+        _dismemberment.Dismember("L_Thigh");
+        _dismemberment.Dismember("L_Calf");
+        
+        #endregion
+
+        #region Upper Body
+
+        _dismemberment.Dismember("Spine");
+        _dismemberment.Dismember("Head");
+        
+        _dismemberment.Dismember("L_Upperarm");
+        _dismemberment.Dismember("L_Forearm");
+        
+        _dismemberment.Dismember("R_Upperarm");
+        _dismemberment.Dismember("R_Forearm");
+
+        #endregion
+        
 
         foreach (var joint in _joints)
         {
